69e5c2f17b5365fe1ee7f5e80f480fa09f605dc9
kobe73er <kobe73er@gmail.com>
2014-03-27 15:43:58 +0800

    fisrt git commit

---
+++ src/packageOne.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author dengpf
+ * @date2014Äê3ÔÂ27ÈÕ
+ */
+public class packageOne {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("first git");
+	}
+
+}
